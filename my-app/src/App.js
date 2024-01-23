4a917a29e803319006da2de49a4d73edbf9fccc6
VladyslavMoiseyenko <vlad.moisenko@gmail.com>
2024-01-23 18:04:09 +0100

    Development MVP Phase 1 v.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "@mui/material/Button";
+
+import Textarea from "@mui/joy/Textarea";
 
 function App() {
   return (
@@ -9,7 +12,7 @@ function App() {
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <p>Hello World!</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +21,43 @@ function App() {
         >
           Learn React
         </a>
+        <div className="area-textfield">
+          {/* <div className="area-textfield__buttton">
+            <Button variant="outlined" disableElevation>
+              Start parcing
+            </Button>
+          </div> */}
+          <div className="area-textfield__input">
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                console.log(event.target[0].value);
+              }}
+            >
+              <Textarea
+                placeholder="Paste your copied HTML code to parse it!"
+                required
+                sx={{ mb: 1 }}
+              />
+
+              <Button type="submit">Start Parcing</Button>
+            </form>
+            {/* <Textarea
+              minRows={2}
+              size="lg"
+              variant="outlined"
+              placeholder="Enter copied Data"
+            /> */}
+          </div>
+        </div>
+
         {/* <p>And now</p> */}
       </header>
+      {/* <body>
+        <Button variant="outlined" disableElevation>
+          Hello World
+        </Button>
+      </body> */}
     </div>
   );
 }
